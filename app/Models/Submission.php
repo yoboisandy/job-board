a638cb913bd01fc0ab7961f8a738f822233fce1f
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2024-08-04 17:20:35 +0545

    [feat] submission model and migration

---
+++ app/Models/Submission.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Submission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'job_listing_id',
+        'user_id',
+        'resume',
+        'cover_letter',
+        'status',
+    ];
+
+    public function jobListing()
+    {
+        return $this->belongsTo(JobListing::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2024_08_04_113051_create_submissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('job_listing_id')->constrained()->cascadeOnDelete();
+            $table->text('resume');
+            $table->text('cover_letter')->nullable();
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submissions');
+    }
+};
